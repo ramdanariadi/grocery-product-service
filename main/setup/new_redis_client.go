bdc7ac06f5f94a6623988109027dd0a341253a81
Ramdan Ariadi <ramadanariadi@gmail.com>
2023-01-01 23:41:04 +0800

    update new_redis_client.go

--- main/setup/new_redis_client.go
+++ main/setup/new_redis_client.go
@@ -1,10 +1,31 @@
 package setup
 
-import "github.com/go-redis/redis/v8"
+import (
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"os"
+	"strings"
+)
 
 func NewRedisClient() *redis.Client {
+	cacheHost := os.Getenv("REDIS_HOST")
+	cachePort := os.Getenv("REDIS_PORT")
+	args := os.Args
+
+	for _, arg := range args {
+		split := strings.Split(arg, "=")
+		switch split[0] {
+		case "REDIS_HOST":
+			cacheHost = split[1]
+			break
+		case "REDIS_PORT":
+			cachePort = split[1]
+			break
+		}
+	}
+
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     fmt.Sprintf("%s:%s", cacheHost, cachePort),
 		Password: "",
 		DB:       0,
 	})
