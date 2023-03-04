88738f5fb94c0e7f4d69e03a429afb144fb40852
Ramdan Ariadi <ramadanariadi@gmail.com>
2023-03-04 15:55:29 +0800

    use global exception handler

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
-	cartModel "github.com/ramdanariadi/grocery-product-service/main/cart/model"
-	categoryModel "github.com/ramdanariadi/grocery-product-service/main/category"
-	"github.com/ramdanariadi/grocery-product-service/main/product/model"
+	cart "github.com/ramdanariadi/grocery-product-service/main/cart/model"
+	"github.com/ramdanariadi/grocery-product-service/main/category"
+	"github.com/ramdanariadi/grocery-product-service/main/exception"
+	"github.com/ramdanariadi/grocery-product-service/main/product"
 	"github.com/ramdanariadi/grocery-product-service/main/setup"
-	transactionModel "github.com/ramdanariadi/grocery-product-service/main/transaction/model"
+	transaction "github.com/ramdanariadi/grocery-product-service/main/transaction/model"
 	"github.com/ramdanariadi/grocery-product-service/main/utils"
-	wishlistModel "github.com/ramdanariadi/grocery-product-service/main/wishlist/model"
+	wishlist "github.com/ramdanariadi/grocery-product-service/main/wishlist/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -18,20 +19,31 @@ func main() {
 	connection, err := setup.NewDbConnection()
 	db, err := gorm.Open(postgres.New(postgres.Config{Conn: connection}))
 	utils.PanicIfError(err)
-	err = db.AutoMigrate(&categoryModel.Category{}, &model.Product{}, &wishlistModel.Wishlist{}, &cartModel.Cart{}, &transactionModel.Transaction{}, &transactionModel.TransactionDetail{})
+	err = db.AutoMigrate(&category.Category{}, &product.Product{}, &wishlist.Wishlist{}, &cart.Cart{}, &transaction.Transaction{}, &transaction.TransactionDetail{})
 	utils.LogIfError(err)
 
 	router := gin.Default()
+	router.Use(gin.CustomRecovery(exception.Handler))
 
 	categoryRoute := router.Group("api/v1/category")
 	{
-		categoryController := categoryModel.NewCategoryController(db)
-		categoryRoute.GET("/", categoryController.FindAll)
-		categoryRoute.GET("/:id", categoryController.FindById)
+		categoryController := category.NewCategoryController(db)
 		categoryRoute.POST("/", categoryController.Save)
+		categoryRoute.GET("/:id", categoryController.FindById)
+		categoryRoute.GET("/", categoryController.FindAll)
 		categoryRoute.PUT("/:id", categoryController.Update)
 		categoryRoute.DELETE("/:id", categoryController.Delete)
 	}
 
-	router.Run()
+	productRoute := router.Group("api/v1/product")
+	{
+		productController := product.NewProductController(db)
+		productRoute.POST("/", productController.Save)
+		productRoute.GET("/:id", productController.FindById)
+		productRoute.GET("/", productController.FindAll)
+		productRoute.PUT("/:id", productController.Update)
+		productRoute.DELETE("/:id", productController.Delete)
+	}
+	err = router.Run()
+	utils.LogIfError(err)
 }
