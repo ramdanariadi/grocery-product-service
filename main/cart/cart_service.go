0de2196105af7d6751b1466f384994c4ba52e84e
Ramdan Ariadi <ramadanariadi@gmail.com>
2023-06-12 21:54:44 +0800

    enhance store cart response

--- main/cart/cart_service.go
+++ main/cart/cart_service.go
@@ -3,7 +3,7 @@ package cart
 import "github.com/ramdanariadi/grocery-product-service/main/cart/dto"
 
 type Service interface {
-	Store(productId string, total uint, userId string)
+	Store(productId string, total uint, userId string) *dto.CartTotalItemDTO
 	Destroy(id string, userId string)
 	Find(reqBody *dto.FindCartDTO) []*dto.Cart
 }
