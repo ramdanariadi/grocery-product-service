bc8de7558d017572bad613e84f8438926bd2a854
Ramdan Ariadi <ramadanariadi@gmail.com>
2023-06-17 15:57:22 +0800

    update main.go

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 	"github.com/ramdanariadi/grocery-product-service/main/exception"
 	"github.com/ramdanariadi/grocery-product-service/main/product"
 	"github.com/ramdanariadi/grocery-product-service/main/setup"
-	transaction "github.com/ramdanariadi/grocery-product-service/main/transaction/model"
+	"github.com/ramdanariadi/grocery-product-service/main/transaction"
+	"github.com/ramdanariadi/grocery-product-service/main/transaction/model"
 	"github.com/ramdanariadi/grocery-product-service/main/user"
 	"github.com/ramdanariadi/grocery-product-service/main/utils"
-	wishlist "github.com/ramdanariadi/grocery-product-service/main/wishlist/model"
+	"github.com/ramdanariadi/grocery-product-service/main/wishlist"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -20,7 +21,7 @@ func main() {
 	connection, err := setup.NewDbConnection()
 	db, err := gorm.Open(postgres.New(postgres.Config{Conn: connection}))
 	utils.PanicIfError(err)
-	err = db.AutoMigrate(&category.Category{}, &product.Product{}, &wishlist.Wishlist{}, &cart.Cart{}, &transaction.Transaction{}, &transaction.TransactionDetail{}, &user.User{})
+	err = db.AutoMigrate(&category.Category{}, &product.Product{}, &wishlist.Wishlist{}, &cart.Cart{}, &model.Transaction{}, &model.TransactionDetail{}, &user.User{})
 	utils.LogIfError(err)
 
 	router := gin.Default()
@@ -32,14 +33,16 @@ func main() {
 		userGroup.POST("/register", userController.Register)
 		userGroup.POST("/login", userController.Login)
 		userGroup.POST("/token", userController.Token)
+		userGroup.PUT("", user.Middleware, userController.Update)
+		userGroup.GET("", user.Middleware, userController.Get)
 	}
 
 	categoryRoute := router.Group("api/v1/category")
 	{
 		categoryController := category.NewCategoryController(db)
-		categoryRoute.POST("/", user.Middleware, categoryController.Save)
+		categoryRoute.POST("", user.Middleware, categoryController.Save)
 		categoryRoute.GET("/:id", categoryController.FindById)
-		categoryRoute.GET("/", categoryController.FindAll)
+		categoryRoute.GET("", categoryController.FindAll)
 		categoryRoute.PUT("/:id", user.Middleware, categoryController.Update)
 		categoryRoute.DELETE("/:id", user.Middleware, categoryController.Delete)
 	}
@@ -47,9 +50,9 @@ func main() {
 	productRoute := router.Group("api/v1/product")
 	{
 		productController := product.NewProductController(db)
-		productRoute.POST("/", user.Middleware, productController.Save)
+		productRoute.POST("", user.Middleware, productController.Save)
 		productRoute.GET("/:id", productController.FindById)
-		productRoute.GET("/", productController.FindAll)
+		productRoute.GET("", productController.FindAll)
 		productRoute.PUT("/:id", user.Middleware, productController.Update)
 		productRoute.DELETE("/:id", user.Middleware, productController.Delete)
 		productRoute.PUT("/top/:id", user.Middleware, productController.SetTopProduct)
@@ -61,7 +64,23 @@ func main() {
 		cartController := cart.NewController(db)
 		cartRoute.POST("/:productId/:total", user.Middleware, cartController.Store)
 		cartRoute.DELETE("/:id", user.Middleware, cartController.Destroy)
-		cartRoute.GET("/", user.Middleware, cartController.Find)
+		cartRoute.GET("", user.Middleware, cartController.Find)
+	}
+
+	wishlistRoute := router.Group("api/v1/wishlist")
+	{
+		wishlistController := wishlist.NewWishlistController(db)
+		wishlistRoute.POST("/:productId", user.Middleware, wishlistController.Store)
+		wishlistRoute.DELETE("/:productId", user.Middleware, wishlistController.Destroy)
+		wishlistRoute.GET("", user.Middleware, wishlistController.Find)
+		wishlistRoute.GET("/:productId", user.Middleware, wishlistController.FindByProductId)
+	}
+
+	transactionGroup := router.Group("api/v1/transaction")
+	{
+		transactionController := transaction.NewTransactionController(db)
+		transactionGroup.POST("", user.Middleware, transactionController.Save)
+		transactionGroup.GET("", user.Middleware, transactionController.Find)
 	}
 
 	err = router.Run()
